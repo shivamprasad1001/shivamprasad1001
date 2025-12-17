2ca2b62ff6e0cd58230cf5d24a40aec497d7d760
Shivam Prasad <shivamprasad1001@gmail.com>
2025-12-17 22:41:43 +0530

    fix: remove incorrect base path causing white screen

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shivamprasad1001/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
